8047d3610ff344037b2033332ce23b373010c7c7
Leonardo G. Ribeiro <leo.ribeiro0256@gmail.com>
2023-02-14 10:11:14 -0300

    commit inicial

---
+++ src/main/java/com/bananas/springtopicos/SpringTopicosApplication.java
@@ -0,0 +1,13 @@
+package com.bananas.springtopicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTopicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTopicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bananas/springtopicos/SpringTopicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bananas.springtopicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTopicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
